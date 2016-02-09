7a78d72b0e01d2bdccd4dbae93d47026023bb73b
Alexianne <alex.d31@free.fr>
2016-02-09 18:18:29 +0100

    modified:   mavenprojectJavaM1/src/main/java/version1/ClientConnect.java

--- mavenprojectJavaM1/src/main/java/version1/ClientConnect.java
+++ mavenprojectJavaM1/src/main/java/version1/ClientConnect.java
@@ -5,17 +5,74 @@
  */
 package version1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.UUID;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.ServerSocket;
+import version1.GestionProtocole;
+/**
 /**
  *
- * @author Alexandra
+ * @author Brice
  */
-public class ClientConnect {
+public class ClientConnect {    
+
+    private Socket socketService;
+    private PrintStream fluxSortieSocket;
+    private BufferedReader fluxEntreeSocket;
+    
 
     /**
-     * @param args the command line arguments
+     *
+     * @param port
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public ClientConnect(int port) // utilisation du 7 par default
+    {
+        try
+        {
+            socketService= new Socket("127.0.0.1",port);// si nim serveur saisissable ATTENTION valeur >65535
+            fluxEntreeSocket = new BufferedReader(new InputStreamReader(socketService.getInputStream()));
+            fluxSortieSocket = new PrintStream(socketService.getOutputStream());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println("Connexion localhost Port :"+port);
+    }
+
+    /**
+     *
+     * @param message
+     * @return
+     * @throws IOException
+     */
+    public String communiquer(String message) throws IOException
+    {
+        //Verification :
+        //System.out.println(message);
+        fluxSortieSocket.println(message);
+        fluxSortieSocket.flush();
+        return fluxEntreeSocket.readLine();
     }
     
+    /**
+     *  Fermeture socket
+     */
+    public void fermer()
+    {
+        try
+        {
+            this.socketService.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
