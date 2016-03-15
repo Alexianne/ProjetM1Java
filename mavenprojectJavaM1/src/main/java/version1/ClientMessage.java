0ca11a6a31f39a0e924cc738b0c2558f491c408b
Bricuuu <balessand@orange.fr>
2016-03-15 14:05:02 +0100

    lblbl

--- mavenprojectJavaM1/src/main/java/version1/ClientMessage.java
+++ mavenprojectJavaM1/src/main/java/version1/ClientMessage.java
@@ -11,7 +11,7 @@ import java.util.Vector;
 
 /**
  *
- * @author Yassine Mahlaoui
+ * @author Xav
  */
 public class ClientMessage {
 
--- mavenprojectJavaM1/src/main/java/version1/ClientThread.java
+++ mavenprojectJavaM1/src/main/java/version1/ClientThread.java
@@ -14,7 +14,7 @@ import java.util.StringTokenizer;
 
 /**
  *
- * @author Yassine Mahlaoui
+ * @author xav
  */
 public class ClientThread extends Thread {
 
